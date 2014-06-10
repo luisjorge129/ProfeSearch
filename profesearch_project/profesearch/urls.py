d9696df4f8cc274c6021eccf72d49bfe8e8a78ff
Luis J. Perez <luisjorge129@gmail.com>
2014-06-09 23:04:41 -0400

    Added static and media to urls.py

--- profesearch_project/profesearch/urls.py
+++ profesearch_project/profesearch/urls.py
@@ -1,7 +1,20 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 urlpatterns = patterns(
     '',
     url(r'^', include('profesearch_project.profesearch.home.urls',
         namespace="home")),
+)
+
+urlpatterns += patterns(
+    '',
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.MEDIA_ROOT})
+)
+
+urlpatterns += patterns(
+    '',
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.STATICFILES_DIRS[0], 'show_indexes': True}),
 )
