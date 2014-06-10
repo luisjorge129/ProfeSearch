4a1b86d074fff900d806e49ad22a4b82b689c363
Luis J. Perez <luisjorge129@gmail.com>
2014-06-10 02:01:27 -0400

    Small changes to settings

--- profesearch_project/profesearch/urls.py
+++ profesearch_project/profesearch/urls.py
@@ -7,14 +7,14 @@ urlpatterns = patterns(
         namespace="home")),
 )
 
-# urlpatterns += patterns(
-#     '',
-#     (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
-#         'document_root': settings.MEDIA_ROOT})
-# )
+urlpatterns += patterns(
+    '',
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.MEDIA_ROOT})
+)
 
-# urlpatterns += patterns(
-#     '',
-#     (r'^static/(?P<path>.*)$', 'django.views.static.serve',
-#         {'document_root': settings.STATICFILES_DIRS[0], 'show_indexes': True}),
-# )
+urlpatterns += patterns(
+    '',
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.STATICFILES_DIRS[0], 'show_indexes': True}),
+)
--- profesearch_project/profesearch/wsgi.py
+++ profesearch_project/profesearch/wsgi.py
@@ -7,6 +7,20 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
+import os
+from os.path import abspath, dirname
+from sys import path
+
+SITE_ROOT = dirname(dirname(abspath(__file__)))
+path.append(SITE_ROOT)
+
+# We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
+# if running multiple sites in the same mod_wsgi process. To fix this, use
+# mod_wsgi daemon mode with each site in its own daemon process, or use
+# os.environ["DJANGO_SETTINGS_MODULE"] = "jajaja.settings"
+os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                      "profesearch.settings.production")
+
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
